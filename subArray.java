0bea5313009abc881f11d07d51d6ee9567246c34
awasthilochan25@gmail.com <awasthilochan25@gmail.com>
2024-04-24 19:13:14 -0500

    sub

--- subArray.java
+++ subArray.java
@@ -1,3 +1,30 @@
 public class subArray {
-    
+
+
+    public static void Sub (int numbers[]){
+
+        for(int i =0; i<numbers.length; i++){
+
+
+            for(int j =i; j<numbers.length; j++){
+
+
+                for(int k=i; k<j; k++){
+
+                    System.out.println(numbers[k]+ "");
+                }//final loops
+                System.out.println();
+
+            }//2nd loop
+                System.out.println();
+        }//1st loop
+        
+    }
+
+    public static void main (String args[]){
+        int numbers[]= {2,4,6,7,8};
+
+        System.err.println("Number" + Sub());
+    }
+
 }
